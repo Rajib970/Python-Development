9b651575cf71d8d997735b17b9d86fd7c11a6a27
Rajib Shaw <rajibshaw582@gmail.com>
2025-11-03 13:04:53 +0530

    Get Todo and Update Todo with authentication

--- FastApi/practice/TodoApp/routers/todos.py
+++ FastApi/practice/TodoApp/routers/todos.py
@@ -27,11 +27,17 @@ class TodoRequest(BaseModel):
     
 @router.get("/", status_code = status.HTTP_200_OK)
 async def read_all(user:user_dependency, db:db_dependency):
+    if user is None:
+        raise HTTPException(status_code=401,detail='Authentication Failed!')
     return db.query(Todos).filter(Todos.owner_id == user.get('id')).all()       
 
 @router.get("/todo/{todo_id}", status_code =  status.HTTP_200_OK)
-async def read_todo(db:db_dependency,todo_id:int = Path(gt=0)):
-    todo_model =  db.query(Todos).filter(Todos.id == todo_id).first()
+async def read_todo(user:user_dependency,db:db_dependency,todo_id:int = Path(gt=0)):
+    if user is None:
+        raise HTTPException(status_code=401,detail='Authentication Failed!')
+     
+    todo_model =  db.query(Todos).filter(Todos.id == todo_id)\
+    .filter(Todos.owner_id == user.get('id')).first()
 
     if todo_model is not None:
         return todo_model
@@ -48,8 +54,12 @@ async def create_todo(user:user_dependency,db:db_dependency, todo_request: TodoR
     return HTTPException(status_code=201,detail='Todo Has Been Created')
 
 @router.put("/todo/{todo_id}",status_code=status.HTTP_204_NO_CONTENT)
-async def update_todo(db:db_dependency,todo_requet:TodoRequest,todo_id:int = Path(gt=0)):
-    todo_model = db.query(Todos).filter(Todos.id ==todo_id).first()
+async def update_todo(user:user_dependency,db:db_dependency,todo_requet:TodoRequest,todo_id:int = Path(gt=0)):
+    if user is None:
+        raise HTTPException(status_code=401,detail='Authentication Failed!')
+    
+    todo_model = db.query(Todos).filter(Todos.id ==todo_id)\
+    .filter(Todos.owner_id == user.get('id')).first()
     if todo_model is None:
         raise HTTPException(status_code=404,detail="Todo Not Found")
     
