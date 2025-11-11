3ac57565fa4b2ac00df9819367151e98408a584a
Rajib Shaw <rajibshaw582@gmail.com>
2025-11-11 15:36:46 +0530

    Admin Routes & PostgreSQL setup an connection

--- FastApi/practice/TodoApp/database.py
+++ FastApi/practice/TodoApp/database.py
@@ -2,9 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+""" Before Working with PostgreSQl, dwonwload the Postgrsql installer and setup DBMS in the system"""
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:rajib@localhost/TodoApplicationDB' 
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL,connect_args = {'check_same_thread':False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SesionLocal = sessionmaker(autocommit=False, autoflush=False,bind=engine)
 
