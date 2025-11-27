4b6226d912c4108e3213236b4210980c6a68e0e2
Rajib Shaw <rajibshaw582@gmail.com>
2025-11-27 15:35:18 +0530

    Path Parameters

---
+++ FastApi/revision/books.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+BOOKS = [
+    {'title':'Title One','author':'Author One','category':'Science'},
+    {'title':'Title Two','author':'Author Two','category':'Science'},
+    {'title':'Title Three','author':'Author Three','category':'Math'},
+    {'title':'Title Four','author':'Author Four','category':'Geography'},
+    {'title':'Title Five','author':'Author Five','category':'Hostory'},
+    {'title':'Title Six','author':'Author Six','category':'Math'}
+]
+
+
+@app.get("/books")
+async def read_all_books():
+    return BOOKS
+
+@app.get("/books/{book_title}")
+async def read_book(book_title:str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
+
+
