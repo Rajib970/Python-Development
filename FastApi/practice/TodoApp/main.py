4e7e161960b8879c85c3edc83285cdc7b403b934
Rajib Shaw <rajibshaw582@gmail.com>
2025-12-16 17:21:31 +0530

    Adding  CSS

--- FastApi/practice/TodoApp/main.py
+++ FastApi/practice/TodoApp/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI, Request
 # import models 
-from .database import engine
-from routers import auth, todos, admin, users
+# from .database import engine
+# from .routers import auth, todos, admin, users
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
@@ -10,6 +11,7 @@ app = FastAPI()
 
 templates = Jinja2Templates(directory="TodoApp/templates")
 
+app.mount("/static",StaticFiles(directory="TodoApp/static"),name="static")
 
 @app.get("/")
 def test(request:Request):
@@ -20,8 +22,8 @@ def test(request:Request):
 def health_check():
     return {'status':'Healthy'}
 
-app.include_router(auth.router)
-app.include_router(todos.router)
-app.include_router(admin.router)
-app.include_router(users.router)
+# app.include_router(auth.router)
+# app.include_router(todos.router)
+# app.include_router(admin.router)
+# app.include_router(users.router)
 
