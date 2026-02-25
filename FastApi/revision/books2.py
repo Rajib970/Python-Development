bc581625bb0db3f0c34aa05a7d8900a26444bfdb
Rajib Shaw <rajibshaw582@gmail.com>
2026-02-25 17:44:24 +0530

    APIs| Assignment solution 6:94

---
+++ FastApi/revision/books2.py
@@ -0,0 +1,110 @@
+from typing import Optional
+from fastapi import FastAPI, Body
+from pydantic import BaseModel , Field
+
+app = FastAPI()
+
+class Book:
+    id: int
+    title: str
+    author: str
+    description: str
+    published_date:int
+    rating: int
+
+
+    def __init__(self,id, title, author, description, published_date,rating):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.published_date = published_date
+        self.rating = rating
+ 
+
+class BookRequest(BaseModel):
+    id:Optional[int] = Field(description="ID is not needed on create", default="None")
+    title:str = Field(min_length=3)
+    author:str = Field(min_length=1)
+    description:str = Field(min_length = 1)
+    published_date:int = Field(gt=0)
+    rating: int = Field(gt=0, lt=6)
+    
+    
+    model_config = {
+        "json_schema_extra":{          
+            "example":{
+                "title" : "A new book",
+                "author" : "codingwithroby",
+                "description": "A new description of a book",
+                "published_date":"2012",
+                "rating":5
+            }
+        }
+    }
+
+
+BOOKS = [
+    Book(1,"OS","Galvin","Concept of OS",2012,5),
+    Book(2,"DBMS","Galvin","Concept of DBMS",2013,5),
+    Book(3,"C++","Galvin","Concept of C++",2014,5),
+    Book(4,"DSA","Galvin","Concept of DSA",2015,5),
+    Book(5,"COA","Galvin","Concept of COA",2016,5)
+]
+
+@app.get('/books')
+async def read_all_books():
+    return BOOKS
+
+@app.get('/books/book_id')
+async def read_book(book_id:int):
+    for book in BOOKS:
+        if book.id == book_id:
+            return book
+        
+@app.get('/books/')
+async def read_book_by_rating(book_rating:int):
+    book_to_return = []
+    for book in BOOKS:
+        if book.rating == book_rating:
+            book_to_return.append(book)
+    return book_to_return        
+
+
+@app.post('/crate-book')
+async def create_book(book_request:BookRequest):
+    new_book = Book(**book_request.model_dump())
+    BOOKS.append(find_book_id(new_book))
+    
+def find_book_id(book:Book):   
+    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1 
+    # if len(BOOKS) > 0:
+    #     book.id = BOOKS[-1].id + 1
+    # else:
+    #     book.id = 1
+    return book    
+
+@app.put("/books/update_book")
+async def update_book(book:BookRequest):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book.id:
+            BOOKS[i] = book
+
+@app.delete('/books/{book_id}')
+async def delete_book(book_id:int):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book_id:
+            BOOKS.pop(i)
+            break
+
+@app.get("/books/published_date")
+async def published_date(date:int):
+    book_to_return = []
+    for book in BOOKS:
+        if book.published_date == date:
+            book_to_return.append(book)
+    return book_to_return        
+
+
+
+
